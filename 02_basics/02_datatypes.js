16a23356cd1c33bfb711fec9e1ee90382e96e693
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-24 06:12:23 +0000

    datatypes

---
+++ 02_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" //treat all JS code as newer version
+
+// alert("Hello") we are using node js not browser
+
+console.log(3+3);
+
+console.log("Anshika"); //code readibility should be high
+
+
+let name="Anshika"
+let age=19
+let isLoggedIn=false
+let state;
+
+//number => 2 to the power 53
+//bigint
+//string =>" "
+//boolean =>true/false
+//null =>stand alone vale
+//undefined  =>
+//symbol =>uniqueness
+
+//object
+
+
+//NULL is a type of OBJECT 
+
+console.log(typeof "Anshika");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
