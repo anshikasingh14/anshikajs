0b920c401b3518ba552f57f3aea88f48fcb7ee7d
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-24 06:56:25 +0000

    Operations

---
+++ 03_basics/03_Operations.js
@@ -0,0 +1,25 @@
+let value=3
+let negValue=-value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//2 to the power 2
+console.log(2/2);
+console.log(2%2);
+
+
+let str="Hello"
+let str1="Anshika"
+
+let str3=str+" "+str1;
+console.log(str3);//Hello Anshika
+
+
+console.log("1"+ 2);//12
+console.log(1 +"2");//12
+console.log("1"+ "2");//12
+console.log("1" +2+ 2);//122
+console.log( 1+ 2+ "2");//122
