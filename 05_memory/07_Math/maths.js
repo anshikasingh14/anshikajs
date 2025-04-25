49b5bd1dde00145581a89cafd5ef0240ed7f8a21
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-25 17:07:32 +0000

    Math

---
+++ 05_memory/07_Math/maths.js
@@ -0,0 +1,34 @@
+const score =400
+
+const balance =new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//100.00
+
+const otherNumber=23.8966
+
+console.loga(otherNumber.toPrecision(3));//124//gives precisely 3 digit number
+console.log(hundreds.toLocalString('en-IN'));
+
+
+
+//+++++++++++++++Mathss+++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));//4
+console.log(Math.ceil(4.3));//5
+console.log(Math.floor(4.9));//4
+console.log(Math.min(4,3,6,7));//3
+console.log(Math.max(4,3,6,7));//7
+console.log(Math.random());//random values between 0 and 1
+console.log(math.floor(Math.random()*10)+1);//to avoid 0.04 like case and to ensure min value is 1
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(math.random() *(max-min +1) + min ))//min value is 14
+//to ensure that minimum this value i need 
