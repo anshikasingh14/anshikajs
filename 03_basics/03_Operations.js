2de8f460d6889f432f173d617cd999c26dcc4432
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-25 13:46:14 +0000

    Operations

--- 03_basics/03_Operations.js
+++ 03_basics/03_Operations.js
@@ -23,3 +23,22 @@ console.log(1 +"2");//12
 console.log("1"+ "2");//12
 console.log("1" +2+ 2);//122
 console.log( 1+ 2+ "2");//122
+
+console.log((3+4) *5 % 3);//don't write such messy code
+
+console.log(true); //true
+
+console.log(+" "); //0
+
+//not valuable
+
+let num1,num2,num3
+
+num1=num2=num3=2+2 //not valuable
+
+//Increment and Decrement
+
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
