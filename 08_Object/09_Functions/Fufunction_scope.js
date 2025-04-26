507dd6da868dcf6a0063e0ecc0433d0e274a1f6e
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-26 18:08:35 +0000

    Functions

--- 08_Object/09_Functions/Fufunction_scope.js
+++ 08_Object/09_Functions/Fufunction_scope.js
@@ -5,7 +5,7 @@ console.log("H");
 console.log("I");
 console.log("K");
 console.log("A");
-*/
+
 
 function sayMyName(){
 console.log("A");
@@ -27,5 +27,60 @@ function addTwoNumbers(num1,num2){
 
 }
 addTwoNumbers(3,"a")
+*/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function calculateCartPrice(val1,val2,...num)//Rest Operator
+{
+  return num
+}
+   
+
+
+console.log(calculateCartPrice(200,400,500,2000))//[ 500, 2000 ]
+
+const user={
+
+    username:"Anshika",
+    price:199,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is 
+        ${anyObject.price}`);//Username is Anshika and price is 
+        //199
+    }
+
+    //handleObject(user)
+    /*
+    handleObject({
+
+        username:"sam",
+        price:399//username is sam and price is 399
+  })
+        */
+
+  const myNewArray=[200,400,100,600]
+
+  function returnSecondValue(getArray){
+
+    return getArray[1]
+  }
+  console.log()
 
