1c342c37a4ecc0b533a9844a81490f1ed8e513d4
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-26 12:23:39 +0000

    JSON

---
+++ 08_Object/object3.js
@@ -0,0 +1,32 @@
+const course={
+    coursename:"JS",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+
+//course.courseInstructor
+
+const{courseInstructor: instructor}=course//name laccording to our wish
+
+//console.log(courseInstructor);//Hitesh
+console.log(instructor);//Hitesh
+
+
+//react part
+//Destructuring of object
+// const navbar =({company}) =>{
+
+//}
+//navbar(company="anshika")
+
+
+//APIs
+
+//Json
+/*
+{
+  "name":"Anshika",
+  "coursename":"Js",
+  "price:":"free",
+}
+  */
