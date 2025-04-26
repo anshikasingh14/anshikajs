25665bd681ec73f7166045bc70cfdcc39acd871f
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-26 12:40:51 +0000

    Functions

---
+++ 08_Object/09_Functions/Fufunction_scope.js
@@ -0,0 +1,31 @@
+/*console.log("A");
+console.log("N");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("K");
+console.log("A");
+*/
+
+function sayMyName(){
+console.log("A");
+console.log("N");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("K");
+console.log("A");
+
+}
+//sayMyName()
+
+
+function addTwoNumbers(num1,num2){
+
+    console.log(num1+num2);
+    
+
+}
+addTwoNumbers(3,"a")
+
+
