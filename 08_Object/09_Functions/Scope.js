d82659fb2b183c154956ef5a081187cbf9d9fc7a
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-27 11:34:15 +0000

    Scope

--- 08_Object/09_Functions/Scope.js
+++ 08_Object/09_Functions/Scope.js
@@ -6,6 +6,7 @@ let a =300
     let a=10
     const b=20
     console.log("Inner:",a);
+    
     }
 
 
@@ -18,3 +19,48 @@ let a =300
 //console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+
+
+//Nested scope
+
+function one()
+{
+    const username="Anshika"
+function two()
+{
+    const website="youtube"
+    console.log(username);//Anshika
+}
+//console.log(website);
+t//wo()
+}
+one()//non executible
+
+if(true){
+    const username="Anshika"
+    if(username=="Anshika")
+    {
+        const website ="youtube"
+        //console.log(username+website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+
+
+//++++++++++Interesting++++++++++++
+
+function addone(num){
+    return num+1
+}
+console.log(addone(5))
+
+
+const addTwo=function(num){
+    return num+2
+}
+
+addTwo(5)
