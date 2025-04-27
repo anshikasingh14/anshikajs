45cf9ffa851cf270ab8b6114f09dd2522a129eab
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-27 13:06:35 +0000

    Changes

---
+++ 08_Object/09_Functions/arrowz-func.js
@@ -0,0 +1,95 @@
+/*console.log("A");
+console.log("N");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("K");
+console.log("A");
+
+
+function sayMyName(){
+console.log("A");
+console.log("N");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("K");
+console.log("A");
+
+}
+//sayMyName()
+
+
+function addTwoNumbers(num1,num2){
+
+    console.log(num1+num2);
+    
+
+}
+addTwoNumbers(3,"a")
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function calculateCartPrice(val1,val2,...num)//Rest Operator
+{
+  return num
+}
+   
+
+
+console.log(calculateCartPrice(200,400,500,2000))//[ 500, 2000 ]
+
+const user={
+
+    username:"Anshika",
+    price:199,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is 
+        ${anyObject.price}`);//Username is Anshika and price is 
+        //199
+    }
+
+    //handleObject(user)
+    /*
+    handleObject({
+
+        username:"sam",
+        price:399//username is sam and price is 399
+  })
+        */
+
+  const myNewArray=[200,400,100,600]
+
+  function returnSecondValue(getArray){
+
+    return getArray[1]
+  }
+  console.log()
+
+  function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is $
+      {anyobject.price}`);
+  }
+  //handleObject(user)
+  handleObject({
+    username:"Sam",
+    price:399
+  })
---
+++ 08_Object/09_Functions/iife.js
@@ -0,0 +1,15 @@
+//Immediately Invoked Function Expression(IIFE)
+
+function chai(){
+    //named iife
+    console.log(DB CONNECTED);
+})();
+//global scope pollution ko hatane ke liye
+// ( function(){
+// console.log((DB CONNECTED TWO))
+
+// })()
+// ( (name) =>{
+//     console.log((DB CONNECTED TWO${name}))
+    
+//     })(Anshika)//DB TWO ANSHIKA
