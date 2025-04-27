d37c94c102077a1b1dc0a43f62e2b6933cd2a8cd
anshikasingh14 <anshikaasingh1426@gmail.com>
2025-04-27 14:39:19 +0000

    GEP

---
+++ 08_Object/09_Functions/codeexze.js
@@ -0,0 +1,43 @@
+//Javascript Exceution Context
+
+
+/*
+        Global Execution Context
+        Function Execution Context
+        Eval Execution context
+
+
+
+
+
+        Memory Creation Phase
+        Execution Phase
+*/
+
+// let val1=10
+// let val2=5
+// function addNum(num1,num2)
+// let total =num1+num2
+// return total
+ 
+// let result1=addNum(val1,val2)
+// let results =addNum(10,2)
+
+
+/*
+val1->undefined
+val2->undefined
+addNum->defination
+result->undefined
+result->undefined
+*/
+
+
+
+/*
+val1<-10
+val2<-5
+*/
+
+
+
